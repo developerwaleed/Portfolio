538f13295d95b6a29d6e669e3f62a5a576ffcb6b
Waleed <waleedamjad99@hotmail.com>
2022-05-09 20:54:35 +0500

    Final Open Menu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const hamburger = document.getElementById('humburger1');
+const hamburger = document.getElementById('humburger');
 const closeBtn = document.getElementById('close');
 const navbar = document.querySelector("#menu-Elements");
 const menuItems = document.querySelectorAll(".menu-item");
@@ -9,8 +9,13 @@ menuItems.forEach(menuItem => menuItem.addEventListener('click', close))
 
 function openMenu() { 
     navbar.style.display = "flex";
+    hamburger.style.display = "none";
+    closeBtn.style.display = "flex";
 }
 
 function close() {
     navbar.style.display = "none";
+    hamburger.style.display = "flex";
+    closeBtn.style.display = "none";
+
 }
