15dcd84b62ef3a331fc4595e86f665ee45f5e1ea
BHDHakim <hakim.bhd@gmail.com>
2022-05-09 14:46:30 +0100

    Add closing menu on link click

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const hamburger = document.getElementById('humburger1');
 const closeBtn = document.getElementById('close');
 const navbar = document.querySelector("#menu-Elements");
+const menuItems = document.querySelectorAll(".menu-item");
 
 hamburger.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", close);
+menuItems.forEach(menuItem => menuItem.addEventListener('click', close))
 
 function openMenu() { 
     navbar.style.display = "flex";
