e4a8c75bc538d869d94dd84c9823ae4d6fdbf11b
Waleed <waleedamjad99@hotmail.com>
2022-05-09 22:01:01 +0500

    Fix Linter Errors

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const closeBtn = document.getElementById('close');
 const navbar = document.querySelector('#menu-Elements');
 const menuItems = document.querySelectorAll('.menu-item');
 
-closeBtn.addEventListener('click', close);
-
-
 function openMenu() {
   navbar.style.display = 'flex';
   hamburger.style.display = 'none';
   closeBtn.style.display = 'flex';
 }
 
-hamburger.addEventListener("click", openMenu);
+hamburger.addEventListener('click', openMenu);
 
 function close() {
   navbar.style.display = 'none';
@@ -20,4 +17,5 @@ function close() {
   closeBtn.style.display = 'none';
 }
 
-menuItems.forEach(menuItem => (menuItem.addEventListener('click', close)));
+closeBtn.addEventListener('click', close);
+menuItems.forEach((menuItem) => {menuItem.addEventListener('click', close)});
